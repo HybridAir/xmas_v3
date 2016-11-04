7c1b5fc25b0193a9c117624e5ed2c70a51cbbf2b
HybridAir <s.tratton.blake@gmail.com>
2016-11-03 21:35:20 -0700

    Cleanup before I start optimizing
    it works perfectly fine right now so this is a good fallback position

--- code/final/charlitest/charlitest.ino
+++ code/final/charlitest/charlitest.ino
@@ -1,7 +1,8 @@
 //basic charlieplex matrix test, goes through all leds
+//compiles to 586 bytes
 #include <util/delay.h>
 
-
+#define NUMOFLEDS 40
 
 unsigned long previousMillis = 0;
 const long interval = 100; // milliseconds gap between sending a network request.
@@ -15,98 +16,55 @@ const long interval = 100; // milliseconds gap between sending a network request
 #define G PA5
 #define H PA6
 
-const int count = 40;
 
-int lit = 0;
 
-// visual array of how the pins are laid out
-int lights[count][2] = {
+// visual array of how the leds are laid out, and the pins they connect to
+//cathode first, then anode
+int ledGrid[NUMOFLEDS][2] = {
 
-      {A,B},  {B,A},  {C,A},  {D,A},  {E,A},  {G,A},  {H,A},  {A,H},
-      
-      {A,C},  {B,C},  {C,B},  {D,B},  {E,B},  {G,B},  {H,B},  {B,H},
+    {A,B},  {B,A},  {C,A},  {D,A},  {E,A},  {G,A},  {H,A},  {A,H},
 
-      {A,D},  {B,D},  {C,D},  {D,C},  {E,C},  {G,C},  {H,C},  {C,H},
-      
-      {A,E},  {B,E},  {C,E},  {D,E},  {E,D},  {G,D},  {H,D},  {D,H},
+    {A,C},  {B,C},  {C,B},  {D,B},  {E,B},  {G,B},  {H,B},  {B,H},
 
-      {A,G},  {B,G},  {C,G},  {D,G},  {E,G},  {G,E},  {H,E},  {E,H}
+    {A,D},  {B,D},  {C,D},  {D,C},  {E,C},  {G,C},  {H,C},  {C,H},
+
+    {A,E},  {B,E},  {C,E},  {D,E},  {E,D},  {G,D},  {H,D},  {D,H},
+
+    {A,G},  {B,G},  {C,G},  {D,G},  {E,G},  {G,E},  {H,E},  {E,H}
 
 };
 
 void setup() {
 
-/*    pinMode(A, INPUT);
-   pinMode(B, INPUT);
-   pinMode(C, INPUT);
-   pinMode(D, INPUT);
-   pinMode(E, INPUT);
-   pinMode(G, INPUT);
-   pinMode(H, INPUT);
-
-   delay(1000); */
-   // while(true) {
-       // light(lights[39]); 
-   // }
+    //individual led test
+    // while(true) {
+        // setLed(ledGrid[39]); 
+    // }
 }
 
 
 
 void loop() {
 
-  for(int i=0;i<count;i++){
-    //if(i == lit){
-      light(lights[i]);
-    //}
-  }
-    
-  //unsigned long currentMillis = millis();
-
-  //if (currentMillis - previousMillis >= interval) {
-    //previousMillis = currentMillis;
-/*     lit++;
-    if(lit == count){
-      lit = 0;
-    } */
-  //}
+    //display all leds
+    for(int i=0;i<NUMOFLEDS;i++){
+        setLed(ledGrid[i]);
+    }
 
 }
 
-void light( int pins[2] ){
 
-  //reset_pins();
+void setLed( int pins[2] ){
+
+  //reset all pins to inputs and LOW
   DDRA = 0;
   PORTA = 0;
-    
-
-  //pinMode( pins[0], OUTPUT );
-  //digitalWrite( pins[0], LOW );
   
-  
-  //pinMode( pins[1], OUTPUT );
-  //digitalWrite( pins[1], HIGH );
-  
+  //set the output pins as outputs
   DDRA |= (1<<pins[0]);
   DDRA |= (1<<pins[1]);
+  
+  //since all pins are currently low, just need one operation set set one high
   PORTA |= (1<<pins[1]);
 }
 
- 
-/* void reset_pins()
-{
-   pinMode(A, INPUT);
-   pinMode(B, INPUT);
-   pinMode(C, INPUT);
-   pinMode(D, INPUT);
-   pinMode(E, INPUT);
-   pinMode(G, INPUT);
-   pinMode(H, INPUT);
-   digitalWrite(A, LOW);
-   digitalWrite(B, LOW);
-   digitalWrite(C, LOW);
-   digitalWrite(D, LOW);
-   digitalWrite(E, LOW);
-   digitalWrite(G, LOW);
-   digitalWrite(H, LOW);
-   
-} */
